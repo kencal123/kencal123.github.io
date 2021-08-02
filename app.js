84ff0c9e3b89a14bb8446262ce83052e519dde89
healye801 <75230941+healye801@users.noreply.github.com>
2021-08-02 15:49:58 -0500

    add index.js

--- app.js
+++ app.js
@@ -45,16 +45,3 @@ db.collection('comments').onSnapshot(snap => {
         }
     });
 });
-function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "<emmaghealy.portfolio@gmail.com>",
-	Password : "<Defender801!>",
-	To : '<emmaghealy.portfolio@gmail.com>',
-	From : "<emmaghealy.portfolio@gmail.com>",
-	Subject : "<Form Submission>",
-	Body : "<Someone has submitted a form on RedbirdCoveStrategies.com.  Please check their response at https://console.firebase.google.com/u/0/project/redbird-cove-comments-draft/firestore/data/~2Fcomments~2FucuHXWeX1KyWHFaELiZx>",
-	}).then(
-		message => alert("mail sent successfully")
-	);
-}
