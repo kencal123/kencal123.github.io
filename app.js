eccb2909efee4e0a7ef37088f7ed38315c9c64e8
healye801 <75230941+healye801@users.noreply.github.com>
2021-08-02 18:18:19 -0500

    debugging app.js

--- app.js
+++ app.js
@@ -9,24 +9,8 @@ form.addEventListener('submit', (e) => {
 	form.email.value = '';
     form.comment.value = '';
     //sendEmail();
-    Email.send({
-        SecureToken : "<86e67c81-19eb-4b48-8267-5d885d9c0754>",
-        //Host: "smtp.gmail.com",
-        //Username : "<emmaghealy.portfolio@gmail.com>",
-        //Password : "<Defender801!>",
-        To : '<healye801@gmail.com>',
-        From : "<emmaghealy.portfolio@gmail.com>",
-        Subject : "<Form Submission>",
-        Body : "<Someone has submitted a form on RedbirdCoveStrategies.com.  Please check their response at https://console.firebase.google.com/u/0/project/redbird-cove-comments-draft/firestore/data/~2Fcomments~2FucuHXWeX1KyWHFaELiZx>",
-        }).then(
-            message => alert("Message sent successfully. Thanks!")
-	);
-    db.collection('comments').add({
-        name: form.name.value,
-		email: form.email.value,
-        comment: form.comment.value
-    });
 });
+form.addEventListener('submit', sendEmail);
 // reading from db
 /*
 const div = document.querySelector('.cont');
