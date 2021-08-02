fd84ee84c6ea837733f2ccc2726ef08291211b2b
healye801 <75230941+healye801@users.noreply.github.com>
2021-08-02 18:09:21 -0500

    debugging app.js

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ const form = document.getElementById('comments');
 // adding data to db
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    form.name.value = '';
+	form.email.value = '';
+    form.comment.value = '';
+    //sendEmail();
     Email.send({
         SecureToken : "<86e67c81-19eb-4b48-8267-5d885d9c0754>",
-    //Host: "smtp.gmail.com",
-	//Username : "<emmaghealy.portfolio@gmail.com>",
-	//Password : "<Defender801!>",
-	To : '<healye801@gmail.com>',
-	From : "<emmaghealy.portfolio@gmail.com>",
-	Subject : "<Form Submission>",
-	Body : "<Someone has submitted a form on RedbirdCoveStrategies.com.  Please check their response at https://console.firebase.google.com/u/0/project/redbird-cove-comments-draft/firestore/data/~2Fcomments~2FucuHXWeX1KyWHFaELiZx>",
-	}).then(
-		message => alert("Message sent successfully. Thanks!")
+        //Host: "smtp.gmail.com",
+        //Username : "<emmaghealy.portfolio@gmail.com>",
+        //Password : "<Defender801!>",
+        To : '<healye801@gmail.com>',
+        From : "<emmaghealy.portfolio@gmail.com>",
+        Subject : "<Form Submission>",
+        Body : "<Someone has submitted a form on RedbirdCoveStrategies.com.  Please check their response at https://console.firebase.google.com/u/0/project/redbird-cove-comments-draft/firestore/data/~2Fcomments~2FucuHXWeX1KyWHFaELiZx>",
+        }).then(
+            message => alert("Message sent successfully. Thanks!")
 	);
     db.collection('comments').add({
         name: form.name.value,
 		email: form.email.value,
         comment: form.comment.value
     });
-    form.name.value = '';
-	form.email.value = '';
-    form.comment.value = '';
-    sendEmail();
 });
 // reading from db
 /*
