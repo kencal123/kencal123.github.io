4133736ecc7503dda08bd1271875df93b4a88ade
healye801 <75230941+healye801@users.noreply.github.com>
2021-08-02 17:11:12 -0500

    debugging email

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //index.js
 function sendEmail() {
 	Email.send({
-	SecureToken : "< 86e67c81-19eb-4b48-8267-5d885d9c0754>",
+	SecureToken : "<86e67c81-19eb-4b48-8267-5d885d9c0754>",
     //Host: "smtp.gmail.com",
 	//Username : "<emmaghealy.portfolio@gmail.com>",
 	//Password : "<Defender801!>",
-	To : '<emmaghealy.portfolio@gmail.com>',
+	To : '<healye801@gmail.com>',
 	From : "<emmaghealy.portfolio@gmail.com>",
 	Subject : "<Form Submission>",
 	Body : "<Someone has submitted a form on RedbirdCoveStrategies.com.  Please check their response at https://console.firebase.google.com/u/0/project/redbird-cove-comments-draft/firestore/data/~2Fcomments~2FucuHXWeX1KyWHFaELiZx>",
