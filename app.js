d398381b2697af8b6d8aa744c2a548c6b1fd3468
healye801 <75230941+healye801@users.noreply.github.com>
2021-08-02 17:36:14 -0500

    debugging email

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ form.addEventListener('submit', (e) => {
     form.name.value = '';
 	form.email.value = '';
     form.comment.value = '';
+    sendEmail();
 });
 // reading from db
 /*
@@ -45,3 +46,17 @@ db.collection('comments').onSnapshot(snap => {
         }
     });
 });
+function sendEmail() {
+	Email.send({
+	SecureToken : "<86e67c81-19eb-4b48-8267-5d885d9c0754>",
+    //Host: "smtp.gmail.com",
+	//Username : "<emmaghealy.portfolio@gmail.com>",
+	//Password : "<Defender801!>",
+	To : '<healye801@gmail.com>',
+	From : "<emmaghealy.portfolio@gmail.com>",
+	Subject : "<Form Submission>",
+	Body : "<Someone has submitted a form on RedbirdCoveStrategies.com.  Please check their response at https://console.firebase.google.com/u/0/project/redbird-cove-comments-draft/firestore/data/~2Fcomments~2FucuHXWeX1KyWHFaELiZx>",
+	}).then(
+		message => alert("Message sent successfully. Thanks!")
+	);
+}
